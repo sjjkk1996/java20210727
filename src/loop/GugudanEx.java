b283e11cdc4845b6f830d32c4fd341cf5cad4337
jjcdesk <sjjkk1996>
2021-07-29 12:11:37 +0900

    0729

--- src/loop/GugudanEx.java
+++ src/loop/GugudanEx.java
@@ -14,12 +14,7 @@ public class GugudanEx {
             }
             System.out.println();
         }
-        for (int i = 2; i < 10; i++) {
-            System.out.printf("%d" + "단 %n", i);
-            for (int j = 1; j < 10; j++) {
-                System.out.printf("%d * %d = %2d %n", i, j, i * j);
-            }
-        }
+
         for (int i = 2; i < 10; i += 3) {
             for (int j = 1; j < 10; j++) {
                 for (int k = 0; k < 3; k++) {
