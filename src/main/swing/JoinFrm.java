a5b8f5e5616fe658f0f5e23a945d32b9c95d9c3d
sjjkk1996 <58585992+sjjkk1996@users.noreply.github.com>
2021-08-13 18:08:15 +0900

    2021 08 13 아마 시험문제인듯

--- src/main/swing/JoinFrm.java
+++ src/main/swing/JoinFrm.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class JoinFrm extends BasicFrm{
     public static void main(String[] args) {new JoinFrm(500, 700, "Join");}
-    private JPanel pnl;
+    private JPanel pnlId, pnlPw, pnlName, pnlBtn;
     private JLabel lbId,lbPw,lbName;
     private JTextField tfId;
     private JTextField tfPw;
@@ -17,29 +17,37 @@ public class JoinFrm extends BasicFrm{
 
     @Override
     public void init() {
-        pnl = new JPanel();
+        pnlId = new JPanel();
+        pnlPw = new JPanel();
+        pnlName = new JPanel();
+        pnlBtn = new JPanel();
 
         lbId = new JLabel("ID: ");
-        add(lbId);
-        tfId = new JTextField();
-        add(tfId);
+        pnlId.add(lbId);
+        tfId = new JTextField(10);
+        pnlId.add(tfId);
         lbPw = new JLabel("PW: ");
-        add(lbPw);
-        tfPw = new JTextField();
-        add(tfPw);
+        pnlPw.add(lbPw);
+        tfPw = new JTextField(10);
+        pnlPw.add(tfPw);
         lbName = new JLabel("NAME: ");
-        add(lbName);
-        tfName = new JTextField();
-        add(tfName);
+        pnlName.add(lbName);
+        tfName = new JTextField(10);
+        pnlName.add(tfName);
         btnJoin = new JButton("Join");
         btnCancel = new JButton("Cancel");
-        add(btnJoin);
-        add(btnCancel);
+        pnlBtn.add(btnJoin);
+        pnlBtn.add(btnCancel);
+        add(pnlId);
+        add(pnlPw);
+        add(pnlName);
+        add(pnlBtn);
 
     }
 
     @Override
     public void arrange() {
+
         setLayout(new GridLayout(4, 2));
     }
 }
